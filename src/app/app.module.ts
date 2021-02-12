29ddf938e33db3c769fbeca021df92264de1f9bd
avinashbirajdar <avinashbirajdar6728@gmail.com>
2021-02-12 13:54:47 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {DataTablesModule } from 'angular-datatables';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DataTablesModule ,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
